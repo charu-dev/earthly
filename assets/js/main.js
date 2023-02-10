78a776ea8f2ff4be91e2ab359d4d06c8b847e897
charu-dev <charu@gupta.com>
2023-02-10 13:39:32 +0530

    purchasebox

--- assets/js/main.js
+++ assets/js/main.js
@@ -59,3 +59,63 @@ function faqsorted(){
 faqsorted()
 
 window.addEventListener('scroll', debounce(checkSlide));
+
+//Up Down arrows for quantity field
+var qtyMin,
+qtyMax,
+qtyField,
+qtyVal;
+$('.icon-minus-squared').on('click', function() {
+qtyField = $(this).next('input[type=number]');
+qtyMin = parseInt($(qtyField).attr('min'));
+qtyVal = parseInt($(qtyField).val());
+if (qtyVal > qtyMin) {
+    qtyVal--;
+    $(qtyField).val(qtyVal);
+    $(this).siblings('.icon-plus-squared').removeClass('off');
+    if (qtyVal === qtyMin) {
+        $(this).addClass('off');
+    }
+}
+
+});
+$('.icon-plus-squared').on('click', function() {
+qtyField = $(this).prev('input[type=number]');
+qtyMax = parseInt($(qtyField).attr('max'));
+qtyVal = parseInt($(qtyField).val());
+if (qtyVal < qtyMax) {
+    qtyVal++;
+    $(qtyField).val(qtyVal);
+    $(this).siblings('.icon-minus-squared').removeClass('off');
+    if (qtyVal === qtyMax) {
+        $(this).addClass('off');
+    }
+}
+
+});
+
+//Validate numeric range of number fields (for quantity input
+   $('input[type=number]').on('blur', function () {
+       var $this = $(this);
+       if ($this.attr('min').length > 0 && $this.attr('max').length > 0) {
+           vQty = parseInt($this.val()),
+           vMin = $this.attr('min'),
+           vMax = $this.attr('max');
+           if (!$.isNumeric(vQty)) {
+               $this.val(vMin);
+             $('.icon-plus-squared').removeClass('off')
+             $('.icon-minus-squared').addClass('off')
+           }
+           else if (vQty < vMin) {
+               $this.val(vMin);
+             $('.icon-plus-squared').removeClass('off')
+             $('.icon-minus-squared').addClass('off')
+           }
+           else if (vQty > vMax) {
+               $this.val(vMax);
+             $('.icon-minus-squared').removeClass('off')
+             $('.icon-plus-squared').addClass('off')
+           }
+           else { return; }
+       }
+   });
